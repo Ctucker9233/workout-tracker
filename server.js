faa50882bab9828a7ad2fc900fb0ee1499ef2eb2
Ctucker9233 <court.m.tucker@gmail.com>
2020-01-29 15:26:28 -0800

    tried moving routes to server js file

--- server.js
+++ server.js
@@ -1,22 +1,81 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
 
-const PORT = process.env.PORT || 3000
+const PORT = process.env.PORT || 3000;
+
+const db = require("./models");
 
 const app = express();
+
+app.use(logger("dev"));
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 app.use(express.static("public"));
 
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost", {
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", {
   useNewUrlParser: true,
   useFindAndModify: false
 });
 
 // routes
-app.use(require("./routes/api-routes.js"));
+var path = require("path");
+
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../workout-tracker/public/index.html"));
+});
+
+app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../workout-tracker/public/exercise.html"));
+});
+
+app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../workout-tracker/public/stats.html"));
+});
+
+app.post("/api/workouts", ({ body }, res) => {
+    db.Workout.create(body)
+        .then(dbWorkout => {
+            console.log(dbWorkout)
+            res.json(dbWorkout);
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+app.get("/api/workouts", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
+
+app.put("/api/workouts/:id", (req, res) => {
+    db.Workout.update(req.body, {
+        where: {
+            _id: req.params.id
+        }
+    })
+        .then(function (dbWorkout) {
+            res.json(dbWorkout);
+        });
+});
+
+app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({})
+        .then(dbWorkout => {
+            res.json(dbWorkout)
+        })
+        .catch(err => {
+            res.status(400).json(err);
+        });
+});
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
